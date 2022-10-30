075b699159ae21972189a324bda4d636ff882758
Susanth Narayanan R <78100230+SusanthNarayanan@users.noreply.github.com>
2022-10-30 18:47:43 +0530

    Add files via upload

---
+++ database.sql
@@ -0,0 +1,22 @@
+create database tourism;
+use tourism;
+
+create table package (
+uid varchar(8) primary key,
+name varchar(30),
+mobile float(12),
+date date,
+boarding_point varchar(50),
+destination varchar(50)
+
+);
+
+create table review (
+userid varchar(8),
+foreign key(userid) references package(uid),
+username varchar(30) ,
+email varchar(50),
+review varchar(500),
+stars int,
+rb varchar(10)
+);
